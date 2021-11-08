8ec2b4653be0a36b6ccf99f4f3cf031508819470
LilianFontalvo <lilianofontalvo99@gmail.com>
2021-11-08 12:17:50 +0100

    Adding cluters

---
+++ src/core/simulation/Cluster.java
@@ -0,0 +1,15 @@
+package core.simulation;
+
+public class Cluster {
+
+    private Point[] points;
+
+    public Cluster(Point[] points){
+        this.points = points;
+    }
+
+    public Point[] getPoints(){
+        return points;
+    }
+    
+}
