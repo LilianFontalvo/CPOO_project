206d5ac01c3e21e35077af9ea3d4665c51516e60
LilianFontalvo <lilianofontalvo99@gmail.com>
2021-12-04 19:33:50 +0100

    Main param + gui

--- src/core/mains/ParamGuiMain.java
+++ src/core/mains/ParamGuiMain.java
@@ -3,14 +3,15 @@ package core.mains;
 import core.generators.LoadingParam;
 import core.simulation.Cluster;
 import core.simulation.Simulation;
+import GUI.Gui;
 
 import java.io.IOException;
 
-public class ParamatersMain {
+public class ParamGuiMain {
     public static void main(String[] args) throws IOException {
         Cluster[] listClusters = LoadingParam.readClusters();
         Simulation sim = new Simulation(listClusters);
-        sim.simOneDay(241, true);
+        new Gui(sim);
     }
     
 }
